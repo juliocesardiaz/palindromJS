7534c0aba2c63c76e7a27dfc355c77d187d0a3e3
Julio Diaz & sam martinez <student@epicodus.com>
2015-09-09 14:25:11 -0700

    add html page and pass information to it

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,22 @@ var palindromes = function(word) {
         return false;
      }
 }
+
+$(document).ready(function() {
+
+    $("form#palindromes").submit(function(event) {
+        var word = ($("input#word").val());
+        var result = palindromes(word);
+        var printOut = "";
+
+        if(result === true) {
+             printOut =  "Yes, it is a Palindrome!";
+        } else {
+            printOut = "No, this is not a Palindrome...";
+        }
+
+        $(".answer").text(printOut);
+        $("#result").show();
+        event.preventDefault();
+    });
+});
